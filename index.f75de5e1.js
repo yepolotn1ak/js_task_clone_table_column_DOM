ac4935b6fbe8552094ee9ac17febfd410765ef8e
Yevhenii <polotnyak2004@gmail.com>
2024-06-05 13:23:38 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,17 @@
+"use strict";
+[
+    ...document.getElementsByTagName("tr")
+].forEach((tr, index, arr)=>{
+    const newTH = document.createElement("th");
+    const newTD = document.createElement("td");
+    if (index === 0 || index === arr.length - 1) {
+        newTH.innerHTML = tr.children[1].innerHTML;
+        tr.append(newTH);
+    } else {
+        newTD.innerHTML = tr.children[1].innerHTML;
+        tr.append(newTD);
+    }
+    tr.append(tr.children[4]);
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
